e9f2caab3792e1159b02fb9ea56afa6e60786cb5
lobo <extremexis1@outlook.com>
2026-07-23 02:10:44 -0400

    Visual feedback to controller/keyboard scrolling
    Additionally fixed bugs and made it fully functional

--- Scripts/start.gd
+++ Scripts/start.gd
@@ -14,7 +14,7 @@ func _ready() -> void:
 	var fade_in = create_tween()
 	fade_in.tween_property($Music, "volume_db", -10, 12).set_trans(Tween.TRANS_LINEAR)
 	$story_text.hide()
-	$BackToMenu.hide()
+	$VBoxContainer2/BackToMenu.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -27,16 +27,19 @@ func _process(_delta: float) -> void:
 		elif select > 1:
 			select -= 1 
 			print(select)
+			set_controller_sprites(select)
 			pass
 	if Input.is_action_just_pressed("scroll_down"):
 		if select == -1:
 			mouse_mode = false
 			select = 1
+			set_controller_sprites(select)
 			Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 			mouse_filter = Control.MOUSE_FILTER_IGNORE
 		elif select >= 1 && select < 4:
 			select += 1 
 			print(select)
+			set_controller_sprites(select)
 			pass
 	if Input.is_action_just_pressed("interact"):
 		match select:
@@ -46,15 +49,18 @@ func _process(_delta: float) -> void:
 					select = 2
 				else: 
 					select = 3
+				set_controller_sprites(select)
 			1:
 				_on_start_pressed()
 			2:
 				_on_story_pressed()
 				select = 0
+				set_controller_sprites(select)
 				story_or_credits = true
 			3:
 				_on_credits_pressed()
 				select = 0
+				set_controller_sprites(select)
 				story_or_credits = false
 			4:
 				_on_quit_pressed()
@@ -64,6 +70,7 @@ func _process(_delta: float) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		print("fart")
 		select = -1
+		reset_sprites()
 
 func _on_start_pressed() -> void:
 	if clickable == true:
@@ -81,7 +88,7 @@ func _on_story_pressed() -> void:
 		$VBoxContainer/Credits.hide()
 		$VBoxContainer/Quit.hide()
 		$story_text.show()
-		$BackToMenu.show()
+		$VBoxContainer2/BackToMenu.show()
 		clickable = false
 		in_sub_menu = true
 
@@ -93,7 +100,7 @@ func _on_back_to_menu_pressed() -> void: # Only pressable while on the story scr
 		$VBoxContainer/Credits.show()
 		$VBoxContainer/Quit.show()
 		$story_text.hide()
-		$BackToMenu.hide()
+		$VBoxContainer2/BackToMenu.hide()
 		$story_text.text = "Kostromvik Union, 198X
 
 After decades, a long abandoned nuclear reactor in a secluded region of the city of Krivichesk has mysteriously shown signs of activity. City records show no entry in or out of the region, or nothing else that would explain it, fueling decades-long whispers of strange happenings in the area. In a matter of three days, activity has grown exponentially and all signs indicate the unmanned reactor is rapidly approaching a meltdown. 
@@ -118,3 +125,72 @@ https://pixabay.com/music/electronic-190501-dark-industrial-ambient-mechanical-e
 \"220626 - Glitch / Dark / Industrial / IDM / Electronica / SF\" by WELC0MEИ0
 https://pixabay.com/music/upbeat-220626-glitch-dark-industrial-idm-electronica-sf-155641/[/left]"
 		_on_story_pressed()
+
+func reset_sprites():
+	$VBoxContainer/Start.texture_normal = load("res://Button Icons/Start.png")
+	$VBoxContainer/Story.texture_normal = load("res://Button Icons/Story.png")
+	$VBoxContainer/Credits.texture_normal = load("res://Button Icons/credits.png")
+	$VBoxContainer/Quit.texture_normal = load("res://Button Icons/quit.png")
+	$VBoxContainer2/BackToMenu.texture_normal = load("res://Button Icons/BackToMenu.png")
+	
+	$VBoxContainer/Start.texture_hover = load("res://Button Icons/Start_hover.png")
+	$VBoxContainer/Story.texture_hover = load("res://Button Icons/Story_hover.png")
+	$VBoxContainer/Credits.texture_hover = load("res://Button Icons/credits_hover.png")
+	$VBoxContainer/Quit.texture_hover = load("res://Button Icons/quit_hover.png")
+	$VBoxContainer2/BackToMenu.texture_hover = load("res://Button Icons/BackToMenu_hover.png")
+	
+func set_controller_sprites(button):
+	match button:
+		0:
+			$VBoxContainer2/BackToMenu.texture_normal = load("res://Button Icons/BackToMenu_hover.png")
+			$VBoxContainer2/BackToMenu.texture_hover = load("res://Button Icons/BackToMenu_hover.png")
+		1:
+			$VBoxContainer/Start.texture_normal = load("res://Button Icons/Start_hover.png")
+			$VBoxContainer/Start.texture_hover = load("res://Button Icons/Start_hover.png")
+			
+			$VBoxContainer/Story.texture_normal = load("res://Button Icons/Story.png")
+			$VBoxContainer/Story.texture_hover = load("res://Button Icons/Story.png")
+			
+			$VBoxContainer/Credits.texture_normal = load("res://Button Icons/credits.png")
+			$VBoxContainer/Credits.texture_hover = load("res://Button Icons/credits.png")
+			
+			$VBoxContainer/Quit.texture_normal = load("res://Button Icons/quit.png")
+			$VBoxContainer/Quit.texture_hover = load("res://Button Icons/quit.png")
+		2:
+			$VBoxContainer/Start.texture_normal = load("res://Button Icons/Start.png")
+			$VBoxContainer/Start.texture_hover = load("res://Button Icons/Start.png")
+			
+			$VBoxContainer/Story.texture_normal = load("res://Button Icons/Story_hover.png")
+			$VBoxContainer/Story.texture_hover = load("res://Button Icons/Story_hover.png")
+			
+			$VBoxContainer/Credits.texture_normal = load("res://Button Icons/credits.png")
+			$VBoxContainer/Credits.texture_hover = load("res://Button Icons/credits.png")
+			
+			$VBoxContainer/Quit.texture_normal = load("res://Button Icons/quit.png")
+			$VBoxContainer/Quit.texture_hover = load("res://Button Icons/quit.png")
+		3:
+			$VBoxContainer/Start.texture_normal = load("res://Button Icons/Start.png")
+			$VBoxContainer/Start.texture_hover = load("res://Button Icons/Start.png")
+			
+			$VBoxContainer/Story.texture_normal = load("res://Button Icons/Story.png")
+			$VBoxContainer/Story.texture_hover = load("res://Button Icons/Story.png")
+			
+			$VBoxContainer/Credits.texture_normal = load("res://Button Icons/credits_hover.png")
+			$VBoxContainer/Credits.texture_hover = load("res://Button Icons/credits_hover.png")
+			
+			$VBoxContainer/Quit.texture_normal = load("res://Button Icons/quit.png")
+			$VBoxContainer/Quit.texture_hover = load("res://Button Icons/quit.png")
+		4: 
+			$VBoxContainer/Start.texture_normal = load("res://Button Icons/Start.png")
+			$VBoxContainer/Start.texture_hover = load("res://Button Icons/Start.png")
+			
+			$VBoxContainer/Story.texture_normal = load("res://Button Icons/Story.png")
+			$VBoxContainer/Story.texture_hover = load("res://Button Icons/Story.png")
+			
+			$VBoxContainer/Credits.texture_normal = load("res://Button Icons/credits.png")
+			$VBoxContainer/Credits.texture_hover = load("res://Button Icons/credits.png")
+			
+			$VBoxContainer/Quit.texture_normal = load("res://Button Icons/quit_hover.png")
+			$VBoxContainer/Quit.texture_hover = load("res://Button Icons/quit_hover.png")
+	pass
+	
