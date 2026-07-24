eb57e476fc08ec80bb1df2b25fc083c985a99506
lobo <extremexis1@outlook.com>
2026-07-24 14:16:08 -0400

    Added controller support to victory screen

--- Scripts/win_screen.gd
+++ Scripts/win_screen.gd
@@ -2,6 +2,8 @@ extends Control
 
 # Simple win screen
 
+var on_controller = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var fade_in = create_tween()
@@ -10,6 +12,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("move_forward"):
+		switch_to_controller()
+	if Input.is_action_just_pressed("scroll_down"):
+		switch_to_controller()
+	if Input.is_action_just_pressed("interact") && on_controller:
+		_on_button_pressed()
+	var mouse_mov = Input.get_last_mouse_velocity().length()
+	if mouse_mov > 0:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		on_controller = false
+		$Button.texture_normal = load("res://Button Icons/BackToMenu.png")
+
+func switch_to_controller():
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
+	on_controller = true
+	$Button.texture_normal = load("res://Button Icons/BackToMenu_hover.png")
 	pass
 
 func _on_button_pressed() -> void:
